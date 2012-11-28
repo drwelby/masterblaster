87e2f0c4871d0e5428109f43f1278a7848e2e7fe
Marc Pfister <marc.pfister@gmail.com>
2012-11-28 11:53:57 -0700

    init

---
+++ static/masterblaster/map.js
@@ -0,0 +1,12 @@
+// create a map in the "map" div, set the view to a given place and zoom
+var map = L.map('map-area').setView([51.505, -0.09], 13);
+
+// add a CloudMade tile layer with style #997
+L.tileLayer('http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'
+}).addTo(map);
+
+// add a marker in the given location, attach some popup content to it and open the popup
+L.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('A pretty CSS3 popup. <br> Easily customizable.').openPopup();
+
---
+++ urls.py
@@ -0,0 +1,23 @@
+from django.conf.urls import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    url(r'^$', 'slc.views.startpage', name='start'),
+    url(r'^new/$', 'slc.views.newmap', name='new'),
+    url(r'^map/([\w-]+)/$', 'slc.views.map_handler', name='map'),
+    url(r'^labels/([\w-]+)/$', 'slc.views.labels', name='labels'),
+    # url(r'^masterblaster/', include('masterblaster.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+)
+
+urlpatterns += staticfiles_urlpatterns()
