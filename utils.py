878e79e0d586b931f87af950fad205095a98bac3
Marc Pfister <marc.pfister@gmail.com>
2013-01-22 04:56:35 -0700

    pdf table output

--- utils.py
+++ utils.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
 from pdflabels import PDFLabel
+from fpdf import FPDF, HTMLMixin
+from cgi import escape
 import xlwt
+
+class HtmlFPDF(FPDF, HTMLMixin):
+        pass
 
 def xls_response(parcels, name):
     xls = xlwt.Workbook()
@@ -68,4 +73,33 @@ def pdf_response(parcels, name, apn=False, address='mail', unique=False, format=
     return response
 
 def pdf_table(parcels, name):
-    return HttpResponse('Coming Soon')
+    pdf = HtmlFPDF(orientation='L',unit='mm',format='LETTER')
+    pdf.set_font('Arial','',8)
+    pdf.add_page()
+    html = "<table>"
+    html += "<thead><tr>"
+    html += '<th width="10%">APN</th>'
+    html += '<th width="30%">OWNER</th>'
+    html += '<th width="20%">SITUS ADDRESS</th>'
+    html += '<th width="40%">MAILING ADDRESS</th>'
+    html += '</tr></thead><tbody>'
+    for parcel in parcels:
+        # hack to make ampersands work in table
+        parcel.owner = (parcel.owner or "").replace("&", "and")
+        if len(parcel.owner) > 35:
+            parcel.owner = parcel.owner[:35] + "..."
+        print parcel.owner
+        html += '<tr>'
+        html += '<td>%s</td>' % parcel.apn
+        html += '<td>%s</td>' % parcel.owner
+        html += '<td>%s</td>' % (parcel.situs1 or " " )
+        html += '<td>%s %s</td>' % (parcel.mail1 or "", parcel.mail2 or "")
+        html += '</tr>'
+    html += '</tbody></table>'
+        
+    pdf.write_html(html)
+    response = HttpResponse(mimetype='application/pdf')
+    # remove this header if you want the pdfs to open in the browser
+    #response['Content-Disposition'] = 'attachment;filename=%s.pdf' % name
+    response.write(pdf.output('','S'))
+    return response
